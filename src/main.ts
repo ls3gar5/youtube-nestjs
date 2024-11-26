c043f5c4b16b841fee0f1f0964f0659162863838
Leo <lsegars@gmail.com>
2024-11-26 09:49:15 -0300

    feat(nest): add new item module

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const configService = app.get(ConfigService);
+  console.log("Port: ",  configService.get('PORT'));
+
+  const config = new DocumentBuilder()
+    .setTitle('My API docs')
+    .setDescription('docs about the API and open methos')
+    .setVersion('1.0')
+    .addTag('items', 'Get all items')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, documentFactory);
+  
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
